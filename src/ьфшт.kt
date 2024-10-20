c92a28fd106cc722d30b175d9e89532476105420
Saydullo <saydullojorahonov24@gmail.com>
2024-10-20 18:37:29 +0500

    fayl yarati va papka yaratish

---
+++ src/ьфшт.kt
@@ -0,0 +1,9 @@
+import java.io.File
+
+fun main() {
+//    val file= File("myfile.txt")
+//    file.writeText("Asalomu alaykum Saydullo ")
+//    file.appendText(" va alaykum assalom")
+    val papka = File("Papka/papka")
+    println(papka.mkdirs())
+}
